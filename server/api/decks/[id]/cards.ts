63c34a01831a0d00a3c362416a3345563eab6342
huraicid <shu.tateuchi@gmail.com>
2026-04-13 21:38:30 +0900

    Add deck cards data and improve card selection query order

--- server/api/decks/[id]/cards.ts
+++ server/api/decks/[id]/cards.ts
@@ -3,7 +3,7 @@ export default defineEventHandler(async (event) => {
   const deckId = getRouterParam(event, 'id')
 
   const { results } = await db.prepare(
-    'SELECT id, card_id, card_name, quantity FROM deck_cards WHERE deck_id = ? ORDER BY card_id'
+    'SELECT id, card_id, card_name, quantity FROM deck_cards WHERE deck_id = ? ORDER BY CAST(SUBSTR(card_id, 1, INSTR(card_id, \'/\') - 1) AS INTEGER)'
   ).bind(deckId).all<{ id: number, card_id: string, card_name: string, quantity: number }>()
 
   return results
